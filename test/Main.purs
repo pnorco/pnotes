c2a25f082a978f8ce42c95f38d9ce15f612c8025
Tiago Dall'Igna <tiago@dalligna.com>
2022-03-30 20:03:25 +0200

    add api test

--- test/Main.purs
+++ test/Main.purs
@@ -4,6 +4,7 @@ import Prelude
 
 import Effect (Effect)
 import Effect.Aff (launchAff_)
+import Server.Api as API
 import Server.Database as DB
 import Test.Spec (describe, it)
 import Test.Spec.Assertions (shouldContain, shouldNotContain)
@@ -22,5 +23,24 @@ main = launchAff_ $ runSpec [consoleReporter] do
     it "delete" do
       let note = { title: "the test note", content: "bla bla bla", createdAt: "2022-03-19T10:20:30"}
       DB.deleteNote "2022-03-19T10:20:30"
+      dbNotes <- DB.getNotes ""
+      dbNotes `shouldNotContain` note
+
+  describe "API" do
+    it "insert" do
+      let noteStr = """
+        { "title": "the test note"
+        , "content": "bla bla bla"
+        , "createdAt": "2022-03-19T10:20:30"
+        }
+      """
+      let noteObj = { title: "the test note", content: "bla bla bla", createdAt: "2022-03-19T10:20:30"}
+      _ <- API.saveNote "" noteStr
+      dbNotes <- DB.getNotes ""
+      dbNotes `shouldContain` noteObj
+
+    it "delete" do
+      let note = { title: "the test note", content: "bla bla bla", createdAt: "2022-03-19T10:20:30"}
+      _ <- API.deleteNote "" "2022-03-19T10:20:30"
       dbNotes <- DB.getNotes ""
       dbNotes `shouldNotContain` note
